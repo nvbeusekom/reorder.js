81044d8f732dc202f08bd734f25883eb6ba48ae3
Beusekom <n.a.c.v.beusekom@tue.nl>
2024-09-16 16:43:02 +0200

    slight cleanup

--- examples/table.js
+++ examples/table.js
@@ -30,81 +30,81 @@ class table{
 	this.col_perm = reorder.permutation(this.m);
     this.col_inv = reorder.inverse_permutation(this.col_perm);
     
-//    var colorLow = 'white', colorHigh = 'black', colorGrid = 'grey';
-//    var max_value = d3.max(this.matrix.map(function(row) { return d3.max(row); })),
-//	color = d3.scale.linear()
-//	    .range([colorLow, colorHigh])
-//	    .domain([0, max_value]);
-//
-//    var gridSize = Math.min(width / this.matrix.length, height / this.matrix[0].length);
-//	this.h = gridSize;
-//	this.th = this.h*this.n;
-//	this.w = gridSize;
-//	this.tw = this.w*this.m;
-//
-//    var h = this.h;
-//    var row_inv = this.row_inv;
-//    var row = this.svg
-//	    .selectAll(".row")
-//	    .data(this.matrix, function(d, i) { return 'row'+i; })
-//	    .enter().append("g")
-//            .attr("id", function(d, i) { return "row"+i; })
-//            .attr("class", "row")
-//            .attr("transform", function(d, i) {
-//		return "translate(0,"+h*row_inv[i]+")";
-//	    });
-//
-//    var w = this.w;
-//    var col_inv = this.col_inv;
-//    var cell = row.selectAll(".cell")
-//	    .data(function(d) { return d; })
-//	    .enter().append("rect")
-//            .attr("class", "cell")
-//            .attr("x", function(d, i) { return w*col_inv[i]; })
-//            .attr("width", w)
-//            .attr("height", h)
-//            .style("fill", function(d) { return color(d); });
-//
-//    row.append("line")
-//	.attr("x2", this.tw)
-//        .style("stroke", colorGrid);
-//
-//    var row_labels = this.row_labels;
-//    
-//    row.append("text")
-//	.attr("x", Math.max(-10,-w/4))
-//	.attr("y", h / 2)
-//	.attr("dy", ".32em")
-//	.attr("text-anchor", "end")
-//        .attr("font-size",Math.min(25,h/2))
-//	.text(function(d, i) { return row_labels[i]; });
-//
-//    var col = this.svg.selectAll(".col")
-//	    .data(this.matrix[0])
-//	    .enter().append("g")
-//	    .attr("id", function(d, i) { return "col"+i; })
-//	    .attr("class", "col")
-//	    .attr("transform", function(d, i) { return "translate(" + w*col_inv[i] + ")rotate(-90)"; });
-//
-//    col.append("line")
-//	.attr("x1", -this.th)
-//        .style("stroke", colorGrid);
-//
-//    var col_labels = this.col_labels;
-//    col.append("text")
-//	.attr("x", w/2)
-//	.attr("y", Math.max(-20,-h/2))
-//	.attr("dy", ".32em")
-//	.attr("text-anchor", "middle")
-//        .attr("transform", "rotate(90)")
-//        .attr("font-size", Math.min(25,h/2))
-//	.text(function(d, i) { return col_labels[i]; });
-//
-//    svg.append("rect")
-//	.attr("width", this.tw)
-//	.attr("height", this.th)
-//	.style("fill", "none")
-//	.style("stroke", colorGrid);
+    var colorLow = 'white', colorHigh = 'black', colorGrid = 'grey';
+    var max_value = d3.max(this.matrix.map(function(row) { return d3.max(row); })),
+	color = d3.scale.linear()
+	    .range([colorLow, colorHigh])
+	    .domain([0, max_value]);
+
+    var gridSize = Math.min(width / this.matrix.length, height / this.matrix[0].length);
+	this.h = gridSize;
+	this.th = this.h*this.n;
+	this.w = gridSize;
+	this.tw = this.w*this.m;
+
+    var h = this.h;
+    var row_inv = this.row_inv;
+    var row = this.svg
+	    .selectAll(".row")
+	    .data(this.matrix, function(d, i) { return 'row'+i; })
+	    .enter().append("g")
+            .attr("id", function(d, i) { return "row"+i; })
+            .attr("class", "row")
+            .attr("transform", function(d, i) {
+		return "translate(0,"+h*row_inv[i]+")";
+	    });
+
+    var w = this.w;
+    var col_inv = this.col_inv;
+    var cell = row.selectAll(".cell")
+	    .data(function(d) { return d; })
+	    .enter().append("rect")
+            .attr("class", "cell")
+            .attr("x", function(d, i) { return w*col_inv[i]; })
+            .attr("width", w)
+            .attr("height", h)
+            .style("fill", function(d) { return color(d); });
+
+    row.append("line")
+	.attr("x2", this.tw)
+        .style("stroke", colorGrid);
+
+    var row_labels = this.row_labels;
+    
+    row.append("text")
+	.attr("x", Math.max(-10,-w/4))
+	.attr("y", h / 2)
+	.attr("dy", ".32em")
+	.attr("text-anchor", "end")
+        .attr("font-size",Math.min(25,h/2))
+	.text(function(d, i) { return row_labels[i]; });
+
+    var col = this.svg.selectAll(".col")
+	    .data(this.matrix[0])
+	    .enter().append("g")
+	    .attr("id", function(d, i) { return "col"+i; })
+	    .attr("class", "col")
+	    .attr("transform", function(d, i) { return "translate(" + w*col_inv[i] + ")rotate(-90)"; });
+
+    col.append("line")
+	.attr("x1", -this.th)
+        .style("stroke", colorGrid);
+
+    var col_labels = this.col_labels;
+    col.append("text")
+	.attr("x", w/2)
+	.attr("y", Math.max(-20,-h/2))
+	.attr("dy", ".32em")
+	.attr("text-anchor", "middle")
+        .attr("transform", "rotate(90)")
+        .attr("font-size", Math.min(25,h/2))
+	.text(function(d, i) { return col_labels[i]; });
+
+    svg.append("rect")
+	.attr("width", this.tw)
+	.attr("height", this.th)
+	.style("fill", "none")
+	.style("stroke", colorGrid);
 
    
 }
